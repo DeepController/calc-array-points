8f19a367e1ef7e084bb5b96859e8f3b19c4e7412
Yang Wang <deepcontrolr@gmail.com>
2017-04-04 16:28:23 -0700

    Initial Commit

---
+++ CalcArrayPoints/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CalcArrayPoints
+//
+//  Created by William on 4/4/17.
+//  Copyright © 2017 Yang Wang. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
